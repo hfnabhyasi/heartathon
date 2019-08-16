5d1f7c660a5c860ee44cc5d89e31fdec01b63f88
Aakash Shah <aakash.shah@publicissapient.com>
2019-08-16 15:17:32 +0530

    Tutorial for team

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,16 @@
 <template>
-  <div id="app" class="container">
-    <h1 class="text-primary">Sample Data : </h1>
-    <div class="row">
-      <template v-for="(card, i) in cardContent"  >
-        <div class="col-3 my-2" :key="i">
-          <TutorialCard :content="card" />
-        </div>
-      </template>
+    <div>
+      <InputWithButton submitText="Submit this form" placeholder="Hello..."/>
+      <InputWithButton submitText="Submit another form..." placeholder="World..."/>
     </div>
-  </div>
 </template>
 
 <script>
-import TutorialCard from './components/TutorialCard'
-import $ from 'jquery'
+import InputWithButton from './components/InputWithButton'
 
 export default {
-  name: 'app',
-  data() {
-    return {
-      cardContent: undefined
-    }
-  },
-  components: {
-    TutorialCard
-  },
-  mounted
-}
-function mounted() {
-  try {
-      $
-        .get('https://jsonplaceholder.typicode.com/posts')
-        .then(res => this.cardContent = res)
-  } catch (e) {
-    // console.error(e);
+  components:{
+    InputWithButton
   }
 }
 </script>
---
+++ src/components/InputWithButton.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div>
+      <input type="text" :placeholder="placeholder" />
+      <input type="button" :value="submitText" />
+  </div>
+</template>
+
+<script>
+export default {
+  props:[ "placeholder", "submitText" ]
+}
+</script>
+
+<style lang="css" scoped>
+</style>
