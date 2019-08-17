c3d85fe8fbeeec4660a50f50fe250b431e142b8b
Aakash Shah <aakash.shah@publicissapient.com>
2019-08-17 16:40:38 +0530

    Working SortDropdown component

---
+++ src/components/SortDropdown.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <div>
+    <select placeholder="Sort By..." @change="onChange($event);">
+      <option value="dateNewest" selected>Date - Newest</option>
+      <option value="dateOldest">Date - Oldest</option>
+      <option value="priceLowToHigh">Price - Low to High</option>
+      <option value="priceHighToLow">Price - High to Low</option>
+    </select>
+  </div>
+</template>
+
+<script>
+import { showError } from '../utils'
+
+export default {
+  methods:{
+    onChange
+  }
+}
+
+function onChange(evt) {
+  try {
+    this.$emit('sortOptionSelected', evt.target.value);
+  } catch (e) {
+    showError.call(e)
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -1,34 +1,75 @@
 <template lang="html">
   <div>
+    <SortDropdown @sortOptionSelected="sortOptionSelected" />
+
     <GridListSwitch
       :isGridActive="true"
       :isListActive="false"
       @toggleGridView="showGridLayout()"
       @toggleListView="showListLayout()"
     />
+
     <div>
         <h1>
           <span v-if="isGrid">Show Grid</span>
           <span v-if="isList">Show List</span>
         </h1>
     </div>
+
+    <pre class="pre pre-scrollable bg-primary text-white">
+      {{ myData }}
+    </pre>
+
   </div>
 </template>
 
 <script>
 import GridListSwitch from './GridListSwitch'
+import SortDropdown from './SortDropdown'
 
 export default {
   data() {
     return {
       isGrid: true,
-      isList: false
+      isList: false,
+      myData: [{
+        createdDate: new Date(Date.now() - (1000 * 60 * 60)),
+        price: 100
+      },{
+        createdDate: new Date(),
+        price: 200
+      }]
     }
   },
-  components: { GridListSwitch },
-  methods: {
-    showGridLayout, showListLayout
-  }
+  components: { GridListSwitch, SortDropdown },
+  methods: { showGridLayout, showListLayout, sortOptionSelected },
+  mounted
+}
+
+function mounted() {
+  // console.log(typeof this.myData)
+  this.myData.sort(sortByDate.bind(this, 'dateNewest'));
+}
+
+function sortOptionSelected(sortOption) {
+  if(sortOption === 'priceLowToHigh')
+    this.myData.sort(sortByPrice.bind(this, sortOption));
+  else if (sortOption === 'priceHighToLow')
+    this.myData.sort(sortByPrice.bind(this, sortOption));
+  else if (sortOption === 'dateOldest')
+    this.myData.sort(sortByDate.bind(this, sortOption));
+  else if (sortOption === 'dateNewest')
+    this.myData.sort(sortByDate.bind(this, sortOption));
+}
+
+function sortByPrice(sortOption, b, a) {
+  if(sortOption == 'priceLowToHigh') return b.price - a.price;
+  else if(sortOption == 'priceHighToLow') return a.price - b.price;
+}
+
+function sortByDate(sortOption, a, b) {
+  if(sortOption == 'dateNewest') return b.createdDate - a.createdDate;
+  else if(sortOption == 'dateOldest') return a.createdDate - b.createdDate;
 }
 
 function showGridLayout() {
