1b9924fa4506396bbbdfb2651be043ab15e35dea
Aakash Shah <aakash.shah@publicissapient.com>
2019-08-17 16:53:37 +0530

    added category-sort-dropdown

---
+++ src/components/CategorySortDropdown.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <div>
+    <select @change="onChange($event);">
+      <option value="all">Categories...</option>
+      <option value="yoga" selected>Yoga</option>
+      <option value="meditation">Meditation</option>
+      <option value="relaxation">Relaxation</option>
+    </select>
+  </div>
+</template>
+
+<script>
+import { showError } from '../utils'
+
+export default {
+  methods:{
+    onChange
+  }
+}
+
+function onChange(evt) {
+  try {
+    this.$emit('categoryOptionSelected', evt.target.value);
+  } catch (e) {
+    showError.call(e)
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
