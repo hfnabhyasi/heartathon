84380f9dac4df3e66b619909fee39474628c7389
ujjwalrastogi <ujjwalengineer29@gmail.com>
2019-08-17 14:15:59 +0530

    change pointer style when hovered over heart

--- src/components/AddToWishList.vue
+++ src/components/AddToWishList.vue
@@ -1,7 +1,6 @@
 <template lang="html">
 <div class="wishlist">
-
-  <i class= "fa-heart" title ="Add to wishlist" :class=" toggleHeart ? 'fas' : 'far' " @click="addToWishList($event)">
+  <i class= "fa-heart h3 c-pointer"  title ="Add to wishlist" :class=" toggleHeart ? 'fas' : 'far' " @click="addToWishList($event)">
   </i>
 </div>
 </template>
@@ -37,4 +36,7 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.c-pointer{
+  cursor:pointer;
+}
 </style>
