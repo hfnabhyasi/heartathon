eb4492c3caf684a4fdccbbc370c8c6055f0abcfa
Aakash Shah <aakash.shah@publicissapient.com>
2019-08-18 13:44:51 +0530

    Working Youtube Video Modal

--- src/components/TestComponent-YouTubeVideoPopup.vue
+++ src/components/TestComponent-YouTubeVideoPopup.vue
@@ -7,20 +7,10 @@
 
     <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
+      <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
           <div class="modal-body">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLT02Kxa-Pd_GbAVYz_v2UGwZm45U0XXcF" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+            <iframe class="w-100" height="500" src="https://www.youtube.com/embed/videoseries?list=PLT02Kxa-Pd_GbAVYz_v2UGwZm45U0XXcF" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </div>
         </div>
       </div>
