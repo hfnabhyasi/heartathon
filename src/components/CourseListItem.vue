cafe1f23ede8728be9d15641d9ad1f0a5c76825c
Aakash Shah <aakash.shah@publicissapient.com>
2019-08-18 16:02:52 +0530

    formatting

--- src/components/CourseListItem.vue
+++ src/components/CourseListItem.vue
@@ -1,35 +1,35 @@
 <template lang="html">
   <div class="border-bottom">
-  <div class="row my-3">
-    <div class="col-3">
-      <img src="https://placehold.it/480x320" alt="heartfulness" class="img-fluid">
-    </div>
-    <div class="col-6">
-
-      <i class="h3 far fa-user-circle"></i>
-
-      <span class="ml-2">{{content.description}}</span>
-
-      <div>
-        <h6>{{content.heading}}</h6>
+    <div class="row my-3">
+      <div class="col-3">
+        <img src="https://placehold.it/480x320" alt="heartfulness" class="img-fluid">
       </div>
+      <div class="col-6">
 
-      <CourseInfo :content="content" />
+        <i class="h3 far fa-user-circle"></i>
 
-      <div>
-        <span>{{content.details}}</span>
+        <span class="ml-2">{{content.description}}</span>
+
+        <div>
+          <h6>{{content.heading}}</h6>
+        </div>
+
+        <CourseInfo :content="content" />
+
+        <div>
+          <span>{{content.details}}</span>
+        </div>
       </div>
-    </div>
-    <div class="col-3 border-left">
-      <div class="text-right">
-      <AddToWishList :content="content"></AddToWishList>
-    <div>  {{content.price}}</div>
-    <RatingA :content="content"></RatingA>
-    <button type="button" class="btn btn-primary my-4">Preview this course</button>
+      <div class="col-3 border-left">
+        <div class="text-right">
+          <AddToWishList :content="content"></AddToWishList>
+          <div> {{content.price}}</div>
+          <RatingA :content="content"></RatingA>
+          <button type="button" class="btn btn-primary my-4">Preview this course</button>
+        </div>
+      </div>
     </div>
   </div>
-</div>
-</div>
 </template>
 
 <script>
